5ba09ea8115ac2b3c6fc97b44e1fbe95cadee7e8
Simon Korzun <simon@korzun.com>
2025-03-06 09:59:05 -0500

    feat: add `Logger` to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { create } from './log';
 export { create } from './log';
-export { Log, Level } from './log.type';
+export { Log, Logger, Level } from './log.type';
 export default create();
